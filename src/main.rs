32f0c2efe2a7d693fd029e692c83f22acbe7d7dd
Rahul Kureel <r.dev2000@gmail.com>
2025-05-11 13:40:34 -0700

    Handle invalid command

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,9 @@ fn main() {
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
+    handle_command(input.as_str());
+}
+
+fn handle_command(command: &str) {
+   println!("{}: command not found", command.trim()); 
 }
