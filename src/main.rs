36d012d35a2a08d8b3da009a336e2af03fb1b305
Rahul Kureel <r.dev2000@gmail.com>
2025-05-11 13:37:20 -0700

    Uncomment code

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
-    // Wait for user input
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 }
