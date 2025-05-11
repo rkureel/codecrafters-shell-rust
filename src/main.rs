8dccdda2e5c5c54ba30bd0fe6d154639388389b6
Rahul Kureel <r.dev2000@gmail.com>
2025-05-11 14:22:44 -0700

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    handle_command(input.as_str());
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        handle_command(input.as_str());
+    }
 }
 
 fn handle_command(command: &str) {
